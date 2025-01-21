9ba18b69ded9d00804c592c49e140ef92a8fc519
KgbArmageddon <karimydesouzaborda@gmail.com>
2025-01-21 01:54:37 -0300

    Adicionado ConversorMoedasApplication.java

---
+++ src/main/java/com/conversor/ConversorMoedasApplication.java
@@ -0,0 +1,11 @@
+package com.conversor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversorMoedasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConversorMoedasApplication.class, args);
+    }
+}
