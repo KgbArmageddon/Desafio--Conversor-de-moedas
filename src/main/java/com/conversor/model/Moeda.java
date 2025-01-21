c390c29f3b8a6bf9582196fc004ab25fdb552e0d
KgbArmageddon <karimydesouzaborda@gmail.com>
2025-01-21 02:02:57 -0300

    Adicionado Moeda.java

---
+++ src/main/java/com/conversor/model/Moeda.java
@@ -0,0 +1,19 @@
+package com.conversor.model;
+
+public class Moeda {
+    private String codigo;
+    private double taxa;
+
+    public Moeda(String codigo, double taxa) {
+        this.codigo = codigo;
+        this.taxa = taxa;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public double getTaxa() {
+        return taxa;
+    }
+}
